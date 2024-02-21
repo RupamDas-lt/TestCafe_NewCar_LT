7563ec6da0a8dc0a00cd1bcd5f0f28c0cd2930f1
Rupam Das <rupamd@LT-MBP-075.local>
2024-02-21 22:47:58 +0530

    Runner files

--- testRunner_iOS14.sh
+++ testRunner_iOS14.sh
@@ -1,4 +1,4 @@
-for i in $(seq 1 2); do
+for i in $(seq 1 20); do
   echo "Iteration $i"
   export PLATFORM_VERSION=iOS14
   testcafe "lambdatest:iPhone 12@14.2:ios" tests/consumer -q --skip-js-errors
--- testRunner_iOS15.sh
+++ testRunner_iOS15.sh
@@ -1,4 +1,4 @@
-for i in $(seq 1 2); do
+for i in $(seq 1 20); do
   echo "Iteration $i"
   export PLATFORM_VERSION=iOS15
   testcafe "lambdatest:iPhone 13@15.0:ios" tests/consumer -q --skip-js-errors
